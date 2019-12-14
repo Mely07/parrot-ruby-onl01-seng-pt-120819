3b91e81603d5698282ce4906c1b97b1b80eb1aea
Damely Tineo <damely.tineo07@gmail.com>
2019-12-14 02:52:08 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot (argument ="Squawk!")
-  puts "Squawk!"
-  return "Squawk!"
+  puts argument
+  return argument
 end
